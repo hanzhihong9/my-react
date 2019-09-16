20f50e233aa611e06f5f8942c0071a6a7c033223
zhihonghan <zhihongh@starstechnologies.com>
2019-09-16 17:31:14 -0400

    add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 const App: React.FC = () => {
   return (
@@ -19,6 +20,9 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 }
